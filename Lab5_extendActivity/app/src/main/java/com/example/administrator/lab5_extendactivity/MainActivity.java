741c9b61f9a902b9b32ab598d5f20299ed83ff15
unknown <307418387@qq.com>
2018-05-01 20:57:38 +0800

    20180501

---
+++ Lab5_extendActivity/app/src/main/java/com/example/administrator/lab5_extendactivity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.lab5_extendactivity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content,new SettingsFragment())
+                .commit();
+
+    }
+}
---
+++ Lab5_extendActivity/app/src/main/java/com/example/administrator/lab5_extendactivity/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.administrator.lab5_extendactivity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SettingsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends PreferenceFragment{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferencexml);
+    }
+
+}
